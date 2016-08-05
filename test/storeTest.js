767df113eccf8a0a00848342d7ce795cab26dcdb
Adriano Raiano <adrai@users.noreply.github.com>
2016-08-05 12:35:44 +0200

    Update storeTest.js

--- test/storeTest.js
+++ test/storeTest.js
@@ -4,7 +4,7 @@ var expect = require('expect.js'),
   _ = require('lodash'),
   crypto = require('crypto');
 
-var types = ['inmemory', 'tingodb', 'mongodb', 'redis'/*, 'azuretable', 'elasticsearch', 'dynamodb'*/];
+var types = ['inmemory', 'tingodb', 'mongodb'/*, 'redis', 'azuretable', 'elasticsearch', 'dynamodb'*/];
 
 var token = crypto.randomBytes(16).toString('hex');
 
